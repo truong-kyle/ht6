e1974b63610639bd9d194bd798920c46f8999d04
Ammar9faisal <134812365+Ammar9faisal@users.noreply.github.com>
2025-07-19 08:55:12 -0400

    added weather data

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { getWeatherData } from './weatherService' 
 
 function App() {
   const [count, setCount] = useState(0)
+  const weatherData = getWeatherData();
+  console.log(weatherData.current.temperature2m); // Example usage of weather data
 
   return (
     <>
